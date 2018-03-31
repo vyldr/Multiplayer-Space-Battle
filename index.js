b07053c66b7efff7be4a4e37dfd08f38e6b8a342
wildesteven <wil14043@byui.edu>
2018-03-31 00:14:52 -0600

    Updated the keypress system

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+global.thevar = [];
